67ec7d49b2277cfa82fd9f6b125d5b989ee66160
Anand <test1122@mailinator.com>
2022-06-21 15:24:35 +0530

    1. App setup & Create functionality

---
+++ server/models/postMessage.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
+});
+
+const PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
+
+// https://www.youtube.com/watch?v=ngc9gnGgUdA&list=PL6QREj8te1P7VSwhrMf3D3Xt4V6_SRkhu&index=1
