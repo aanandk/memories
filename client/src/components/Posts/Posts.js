acb11af9adab17a10b328da9b994532bf66465a0
Anand <test1122@mailinator.com>
2022-06-22 18:05:19 +0530

    2. Update, Delete, Like, UI & Misc

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,16 +1,23 @@
 import { useSelector } from 'react-redux';
+import { CircularProgress, Grid } from '@material-ui/core';
 
+import useStyles from './styles';
 import Post from "./Post/Post";
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
     const posts = useSelector(state => state.posts)
-    console.log(posts)
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>Posts</h1>
-            <Post />
-            <Post />
-        </div>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {posts.map(post => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} setCurrentId={setCurrentId} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     )
 }
 
